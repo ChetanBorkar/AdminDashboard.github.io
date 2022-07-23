8c5f8f0335df64578e584eda917952e4ce9c48e0
ChetanBorkar <dchetanb99@gmail.com>
2022-07-23 17:45:23 +0530

    adminDashboard

---
+++ js/oders.js
@@ -0,0 +1,56 @@
+const Orders = [
+    {
+        productName: 'Calcium Syrup',
+        ProductNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Cheston cold & Flue ',
+        ProductNumber: '25682',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lomo D',
+        ProductNumber: '112254',
+        paymentStatus: 'Due',
+        shipping: 'Denied'
+    },
+    {
+        productName: 'Cough Syrup',
+        ProductNumber: '756825',
+        paymentStatus: 'Paid',
+        shipping: 'Shipped'
+    },
+    {
+        productName: 'Multivitamin Syrup',
+        ProductNumber: '66325',
+        paymentStatus: 'Paid',
+        shipping: 'Shipped'
+    },
+    {
+        productName: 'Titanus Injection',
+        ProductNumber: '457852',
+        paymentStatus: 'Due',
+        shipping: 'Denied'
+    },
+    {
+        productName: 'Aldigesic SP',
+        ProductNumber: '8885522',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'ORS Powder',
+        ProductNumber: '663251',
+        paymentStatus: 'Paid',
+        shipping: 'Shipped'
+    },
+    {
+        productName: 'Victofol Syrup',
+        ProductNumber: '7541250',
+        paymentStatus: 'Due',
+        shipping: 'Denied'
+    }
+]
